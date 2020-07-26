36741ae5dc4b66639546ff0e52c55c111423cf6f
cinusss <cendrowskimarcin2@gmail.com>
2020-07-26 11:11:39 +0100

    combo

---
+++ ComboOpener/ComboOpener/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace ComboOpener
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(ConfigurationManager.AppSettings["plik1"]);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(ConfigurationManager.AppSettings["plik2"]);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(ConfigurationManager.AppSettings["plik3"]);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(ConfigurationManager.AppSettings["plik4"]);
+        }
+    }
+}
---
+++ OpenDynamicFiles.bat
@@ -0,0 +1,2 @@
+@echo off
+FOR %%f IN ("C:\Users\01034497\Desktop\BUDZ\*.xlsx") DO start "C:\Program Files (x86)\Microsoft Office\root\Office16\EXCEL.EXE" "%%~f"
